c78278a6cdb4b3572ca86ef87caa6b161054a8a7
sokkyyy <ndegwaraymond@gmail.com>
2019-12-04 11:19:18 +0300

    Added a React Component for the registration of users.

--- visitkenya-frontend/src/auth/Register.js
+++ visitkenya-frontend/src/auth/Register.js
@@ -16,7 +16,31 @@ export default class Registration{
               <div className="four wide column"></div>
               <div className="four wide column"></div>
               <div className="four wide column"></div>
-              <div className="eight wide column"></div>
+              
+              {/* Registration Form */}
+              <div className="eight wide column">
+                  <form onSubmit={this.handleSubmit} className='ui form'>
+                    <div className="field">
+                      <label>Full Name</label>
+                      <input type="text" name="first-name" placeholder="First Name" ref='fullName' />
+                    </div>
+                    <div className="field">
+                      <label>Username</label>
+                      <input type="text" name="first-name" placeholder="First Name" ref='username' />
+                    </div>
+                    <div className="field">
+                      <label>Email</label>
+                      <input type="email" name="first-name" placeholder="First Name" ref='email' />
+                    </div>
+                    <div className="field">
+                      <label>Password</label>
+                      <input type="password" name="first-name" placeholder="First Name" ref='password' />
+                    </div>
+                    <button className="ui black basic button" type='submit'>Sign Up</button>
+                  </form>
+              </div>
+
+
               <div classNamess="four wide column"></div>
             </div>
         );
